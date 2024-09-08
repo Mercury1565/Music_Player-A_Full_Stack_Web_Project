d95ad06ed129d85e7e0c347f938662a8e2422f59
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-09-09 01:58:29 +0300

    integrate cloudinary

--- back-end/api/main.go
+++ back-end/api/main.go
@@ -1,38 +1,40 @@
-// package handler
-package main
+package handler
+
+// package main
 
 import (
 	"music_player_backend/config"
 	"music_player_backend/delivery/routers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// var ginEngine *gin.Engine
-// var app config.Application
+var ginEngine *gin.Engine
+var app config.Application
 
-// func init() {
-// 	app = config.App()
-// 	env := app.Env
-
-// 	ginEngine = gin.New()
-// 	database := app.Mongo.Database(env.DB_NAME)
-
-// 	routers.Setup(env, *database, ginEngine)
-// }
-
-// func Handler(w http.ResponseWriter, r *http.Request) {
-// 	ginEngine.ServeHTTP(w, r)
-// }
-
-func main() {
-	app := config.App()
+func init() {
+	app = config.App()
 	env := app.Env
-	gin := gin.Default()
 
+	ginEngine = gin.New()
 	database := app.Mongo.Database(env.DB_NAME)
-	defer app.CloseMongoDBConnection()
 
-	routers.Setup(env, *database, gin)
-	gin.Run(env.SERVER_ADDRESS)
+	routers.Setup(env, *database, ginEngine)
 }
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	ginEngine.ServeHTTP(w, r)
+}
+
+// func main() {
+// 	app := config.App()
+// 	env := app.Env
+// 	gin := gin.Default()
+
+// 	database := app.Mongo.Database(env.DB_NAME)
+// 	defer app.CloseMongoDBConnection()
+
+// 	routers.Setup(env, *database, gin)
+// 	gin.Run(env.SERVER_ADDRESS)
+// }

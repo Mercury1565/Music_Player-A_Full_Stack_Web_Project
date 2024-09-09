fce7c7d3e57b5be4763b4d9bb92dbf602bf5a938
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-09-10 01:38:10 +0300

    fix main.go

--- back-end/api/main.go
+++ back-end/api/main.go
@@ -1,7 +1,5 @@
 package handler
 
-// package main
-
 import (
 	"music_player_backend/config"
 	"music_player_backend/delivery/routers"
@@ -26,15 +24,3 @@ func init() {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	ginEngine.ServeHTTP(w, r)
 }
-
-// func main() {
-// 	app := config.App()
-// 	env := app.Env
-// 	gin := gin.Default()
-
-// 	database := app.Mongo.Database(env.DB_NAME)
-// 	defer app.CloseMongoDBConnection()
-
-// 	routers.Setup(env, *database, gin)
-// 	gin.Run(env.SERVER_ADDRESS)
-// }
--- front-end/src/api/apiClient.js
+++ front-end/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://music-player-a-full-stack-web-project-e2f7.vercel.app';
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://music-player-a-full-stack-web-project-e2f7.vercel.app';
+// const API_BASE_URL = 'http://localhost:8080';
 
 const api = axios.create({
   baseURL: API_BASE_URL,

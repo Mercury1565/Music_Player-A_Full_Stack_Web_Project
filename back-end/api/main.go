618a23dfa1a79d7ed0c98765a76b052e37a7ad9a
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-09-10 02:44:57 +0300

    fix main.go

--- back-end/api/main.go
+++ back-end/api/main.go
@@ -1,26 +1,39 @@
 package handler
 
 import (
-	"music_player_backend/config"
-	"music_player_backend/delivery/routers"
-	"net/http"
+    "music_player_backend/config"
+    "music_player_backend/delivery/routers"
+    "net/http"
+    "os"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 var ginEngine *gin.Engine
 var app config.Application
 
 func init() {
-	app = config.App()
-	env := app.Env
+    app = config.App()
+    env := app.Env
 
-	ginEngine = gin.New()
-	database := app.Mongo.Database(env.DB_NAME)
+    ginEngine = gin.New()
+    database := app.Mongo.Database(env.DB_NAME)
 
-	routers.Setup(env, *database, ginEngine)
+    routers.Setup(env, *database, ginEngine)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	ginEngine.ServeHTTP(w, r)
+    ginEngine.ServeHTTP(w, r)
+}
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "3000" // Default to port 3000 if no port is specified
+    }
+
+    err := http.ListenAndServe(":" + port, ginEngine)
+    if err != nil {
+        panic(err)
+    }
 }

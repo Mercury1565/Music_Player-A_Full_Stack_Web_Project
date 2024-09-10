81093b7433d97a2ace56cbca2f5df8324d249b37
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-09-10 16:32:29 +0300

    fix fwd and bwd, fix phone styling

--- front-end/src/api/apiClient.js
+++ front-end/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://music-player-a-full-stack-web-project-6xse.vercel.app';
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://music-player-a-full-stack-web-project-6xse.vercel.app';
+// const API_BASE_URL = 'http://localhost:8080';
 
 const api = axios.create({
   baseURL: API_BASE_URL,

59a060886ef557d8808ed5da62c2b2b94b65d93d
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-09-19 17:19:23 +0300

    Update main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+
+import rootSaga from './redux/sagas';  
+import rootReducer from './redux/store.js'; 
+
+// Create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Configure redux store
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),  
+});
+
+sagaMiddleware.run(rootSaga);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
